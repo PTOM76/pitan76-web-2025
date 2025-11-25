ef7b45fadeb370e1bb9e3df297792dcb2c6ac3ba
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-11-25 16:09:16 +0900

    Update HTML files and CSS styles for improved layout and functionality
    - Added favicon link to index.html files in the links and works sections.
    - Updated script references in index.html files to reflect new build IDs.
    - Modified CSS in page.module.css to use a responsive max-width for better adaptability on various screen sizes.

--- out/_next/static/chunks/cc2beec55743551d.js
+++ out/_next/static/chunks/cc2beec55743551d.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/f2e722c6addc5a6b.js",
+  "static/chunks/58e98a63b5e1c025.js",
   "static/chunks/894332fd138781fb.js",
   "static/chunks/9e6fbe3548b9640c.css",
-  "static/chunks/turbopack-53f93a01ff49fd35.js"
+  "static/chunks/turbopack-6ddbc9be6ed7c8ca.js"
 ])
